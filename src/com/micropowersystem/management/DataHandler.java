7b632ff500183b0c75a0b7808a3ab43ec2367799
THU-XuHan <66936100+THU-XuHan@users.noreply.github.com>
2020-06-17 14:27:24 +0800

    Update DataHandler.java

--- src/com/micropowersystem/management/DataHandler.java
+++ src/com/micropowersystem/management/DataHandler.java
@@ -3,5 +3,7 @@ package com.micropowersystem.management;
 import java.util.ArrayList;
 
 public interface DataHandler {
-	public void OnDataChanged(ArrayList<Double> Prices);
+	
+	public void OnDataChanged(ArrayList<Double> Prices);//Income£¬Cost£¬Selling amount, Buying Amount
+	
 }
