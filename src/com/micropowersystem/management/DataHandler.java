ad4770cd0ca6d2a2aed16e89d622a720543dc249
THU-XuHan <66936100+THU-XuHan@users.noreply.github.com>
2020-06-17 13:48:58 +0800

    更新了Interface

---
+++ src/com/micropowersystem/management/DataHandler.java
@@ -0,0 +1,7 @@
+package com.micropowersystem.management;
+
+import java.util.ArrayList;
+
+public interface DataHandler {
+	public void OnDataChanged(ArrayList<Double> Prices);
+}
---
+++ src/com/micropowersystem/management/Management.java
@@ -0,0 +1,37 @@
+package com.micropowersystem.management;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+import org.jfree.data.time.TimeSeries;
+
+public interface Management {
+	
+	final int GENERATOR = 0;
+	final int STORAGE = 1;
+	final int USER = 2;
+	
+	public boolean getManagerStatus();
+	
+	//以下三个函数需要manager返回  各个类型设备所包含的设备名称，名称是这些设备的唯一标识
+	public Vector<String> getNames(int TYPE);
+	
+	//返回指定类型的设备的实时功率值，String对应的是设备的标识
+	public HashMap<String,TimeSeries> getPowerTimeSeries(int TYPE);
+	public HashMap<String,TimeSeries> getVoltageTimeSeries(int TYPE);
+	
+	//
+	public TimeSeries getTotalPowerTimeSeries(int TYPE);
+	
+	
+	
+	
+	public HashMap<String,TimeSeries> getForecastPower();
+	
+	
+	public void sendEmail();
+	
+	
+	
+	
+}
