7601f1e22942c1836004698c6343b9573d947594
suozhangHuang <huangjl17@mails.tsinghua.edu.cn>
2020-06-18 15:30:24 +0800

    Update Email.java

--- src/com/micropowersystem/management/Email.java
+++ src/com/micropowersystem/management/Email.java
@@ -118,13 +118,13 @@ public class Email
 	// 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
 	// PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
 	// 对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-	private static String myEmailAccount = "huangjl17@mails.tsinghua.edu.cn";
-	private static String myEmailPassword = "bf3725c42d4b43e8";
+	private String myEmailAccount = "huangjl17@mails.tsinghua.edu.cn";
+	private String myEmailPassword = "bf3725c42d4b43e8";
 
 	// 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
 	// 网易126邮箱的 SMTP 服务器地址为: smtp.126.com
-	private static String myEmailSMTPHost = "smtp.tsinghua.edu.cn";
+	private String myEmailSMTPHost = "smtp.tsinghua.edu.cn";
 
 	// 收件人邮箱（替换为自己知道的有效邮箱）
-	private static String receiveMailAccount = "huangjl17@mails.tsinghua.edu.cn";
+	private String receiveMailAccount = "huangjl17@mails.tsinghua.edu.cn";
 }
